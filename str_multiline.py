e48dd0e216788a9dc4b106b88f40be7f52403130
anjal@academiacollege.edu.np <anjal@academiacollege.edu.np>
2023-05-18 12:37:46 +0545

    Variables

---
+++ str_multiline.py
@@ -0,0 +1,5 @@
+#Three invertad commas for more than 1 line string
+print('''Hello, how are you
+where are you?
+Why are you?
+when are you?''')
---
+++ var.py
@@ -0,0 +1,10 @@
+#String concatenation
+myName="My name is Anjal"
+myAge="and i am 25 years old"
+myAddress="and i live in Godawari"
+print(myName+myAge+myAddress)
+#String interpolation
+name="anjal"
+age=25
+Address="Godawari"
+print(f"\nMy name is {name} and I am {age} years old and I live in {Address}.")
