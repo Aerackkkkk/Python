882cc6eeaf74c947804a8de12ba0c4ea5a8b31ff
anjal@academiacollege.edu.np <anjal@academiacollege.edu.np>
2023-05-23 22:14:39 +0545

    Exercise

--- exercise.py
+++ exercise.py
@@ -1,4 +1,4 @@
-#python exercises
+#python exercises pre defined
 #1. Declare a variable "name" and assign your name to it and display the name
 name="Anjal Thapa"
 print(f'my name is {name}')
@@ -7,3 +7,8 @@ num1=200
 num2=450
 sum=num1+num2
 print(sum)
+#3. Rectangle and area calculation
+length=20
+breadth=40
+area=length*breadth
+print(f"The area of rectangle is {area}.")
