d57a816ceed65d52c46d0dbf640ba756286cf384
anjal@academiacollege.edu.np <anjal@academiacollege.edu.np>
2023-05-24 19:58:12 +0545

    userinput

---
+++ input.py
@@ -0,0 +1,9 @@
+#Taking input from user
+name=input("Hello mr/ms what is your father put is your name?\n")
+# print(f"Hello {name} how are you today?")
+age=input("Please kindly enter your age in years.\n")
+age=int(age)
+year=2023-age
+address=input("Can you tell us where do you live now?\n")
+email=input("Please enter your email address\n")
+print(f"Hello {name} of {address} how are you today? Based on your input your age is {year} years old. We will contact you on {email}")
